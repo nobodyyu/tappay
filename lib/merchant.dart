174f96e0d0c3237ecfac39ffc590060ff0c7594f
eason4best <eason4best@gmail.com>
2023-12-25 15:27:58 +0800

    Minor Improve

--- lib/merchant.dart
+++ lib/merchant.dart
@@ -4,4 +4,20 @@ enum Merchant {
   jkoPay,
   easyWallet,
   googlePay;
+
+  @override
+  String toString() {
+    switch (this) {
+      case Merchant.directPay:
+        return '信用卡/簽帳卡';
+      case Merchant.linePay:
+        return 'Line Pay';
+      case Merchant.jkoPay:
+        return '街口支付';
+      case Merchant.easyWallet:
+        return '悠遊卡錢包';
+      case Merchant.googlePay:
+        return 'Google Pay';
+    }
+  }
 }
